da4f22bcbc28cda14e18267744990052149ef97f
RoshanSawlani <roshansawlani@gmail.com>
2024-07-17 18:51:12 +0530

    Create form for uploading product in product section of admin panel

--- frontend/src/pages/AllProducts.js
+++ frontend/src/pages/AllProducts.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UploadProduct from '../components/UploadProduct'
 
 const AllProducts = () => {
+  const [openUploadProduct,setOpenUploadProduct] = useState(false)
   return (
-    <div>AllProducts</div>
+    <div>
+      <div className="bg-white py-2 px-4 flex justify-between items-center">
+        <h2 className='text-bold text-lg'>All Products</h2>
+        <button className='border-2 border-green-600 text-green-600 hover:bg-green-600 hover:text-white transition-all py-1 px-3 rounded-full' onClick={()=>setOpenUploadProduct(true)}>Upload Product</button>
+      </div>
+      {/* Upload Product component */}
+      {
+        openUploadProduct && (
+          <UploadProduct onClose={()=>setOpenUploadProduct(false)}/>
+        )
+      }
+    </div>
   )
 }
 
