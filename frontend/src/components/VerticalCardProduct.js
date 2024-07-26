74fc9dc79ce9387eda925019ac7d18f58c7b339b
RoshanSawlani <roshansawlani@gmail.com>
2024-07-26 15:59:31 +0530

    Displayed product in Vertical Card

--- frontend/src/components/VerticalCardProduct.js
+++ frontend/src/components/VerticalCardProduct.js
@@ -3,7 +3,7 @@ import fetchCategoryWiseProduct from '../helpers/fetchCategoryWiseProduct'
 import displayINRCurrency from '../helpers/displayCurrency'
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa6'
 
-const HorizontalCardProduct = ({ category, heading }) => {
+const VerticalCardProduct = ({ category, heading }) => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
 
@@ -32,35 +32,36 @@ const HorizontalCardProduct = ({ category, heading }) => {
     return (
         <div className='container mx-auto px-4 my-6 relative'>
             <h2 className='text-2xl font-semibold py-4'>{heading}</h2>
-            <div className='flex items-center gap-4 md:gap-6 overflow-scroll scrollbar-none transition-all' ref={scrollElement}>
+            <div className='flex items-center gap-4 md:gap-6 overflow-x-scroll scrollbar-none transition-all' ref={scrollElement}>
                 <button className='bg-white shadow-md rounded-full p-1 absolute left-0 text-lg hidden md:block' onClick={scrollLeft}><FaAngleLeft /></button>
                 <button className='bg-white shadow-md rounded-full p-1 absolute right-0 text-lg hidden md:block' onClick={scrollRight}><FaAngleRight /></button>
                 {loading ? (
                     loadingList.map((product, index) => {
                         return (
-                            <div className='w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] h-36 bg-white rounded-sm shadow flex'>
-                                <div className='bg-slate-200 h-full p-4 min-w-[120px] md:min-w-[145px] animate-pulse'>
+                            <div className='w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px]bg-white rounded-sm shadow'>
+                                <div className='bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse'>
+                                    
                                 </div>
-                                <div className='p-4 grid w-full gap-2'>
-                                    <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black bg-slate-200 animate-pulse p-1 rounded-full'></h2>
-                                    <p className='capitalize text-slate-500 animate-pulse p-1 rounded-full'></p>
-                                    <div className='flex gap-3 w-full'>
-                                        <p className='text-green-600 font-medium animate-pulse p-1 rounded-full bg-slate-200 w-full '></p>
-                                        <p className='text-slate-500 line-through animate-pulse p-1 rounded-full bg-slate-200 w-full'></p>
+                                <div className='p-4 grid gap-3'>
+                                    <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animation-pulse rounded-full bg-slate-200'></h2>
+                                    <p className='capitalize text-slate-500 p-1 animation-pulse rounded-full bg-slate-200 py-2'></p>
+                                    <div className='flex gap-3'>
+                                        <p className='text-green-600 font-medium p-1 animation-pulse rounded-full bg-slate-200 w-full py-2'></p>
+                                        <p className='text-slate-500 line-through p-1 animation-pulse rounded-full bg-slate-200 w-full py-2'></p>
                                     </div>
-                                    <button className='text-sm text-white px-3 py-0.5 animate-pulse p-1 rounded-full w-full bg-slate-200'></button>
+                                    <button className='text-sm text-white px-3 animation-pulse rounded-full bg-slate-200 py-2'></button>
                                 </div>
                             </div>
                         )
                     })
-                ) :
-                    (data.map((product, index) => {
+                ) : (
+                    data.map((product, index) => {
                         return (
-                            <div className='w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] h-36 bg-white rounded-sm shadow flex'>
-                                <div className='bg-slate-200 h-full p-4 min-w-[120px] md:min-w-[145px]'>
-                                    <img src={product.productImage[0]} className='object-scale-down h-full hover:scale-110 transition-all' alt="" />
+                            <div className='w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px]bg-white rounded-sm shadow'>
+                                <div className='bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center'>
+                                    <img src={product.productImage[0]} className='object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply' alt="" />
                                 </div>
-                                <div className='p-4 grid'>
+                                <div className='p-4 grid gap-3'>
                                     <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black'>{product?.productName}</h2>
                                     <p className='capitalize text-slate-500'>{product?.category}</p>
                                     <div className='flex gap-3'>
@@ -79,4 +80,4 @@ const HorizontalCardProduct = ({ category, heading }) => {
     )
 }
 
-export default HorizontalCardProduct
+export default VerticalCardProduct
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CategoryList from '../components/CategoryList'
 import BannerProduct from '../components/BannerProduct'
 import HorizontalCardProduct from '../components/HorizontalCardProduct'
+import VerticalCardProduct from '../components/VerticalCardProduct'
 
 const Home = () => {
     return (
@@ -9,6 +10,17 @@ const Home = () => {
             <CategoryList/>
             <BannerProduct/>
             <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpods"}/>
+            <HorizontalCardProduct category={"watches"} heading={"Popular's Watches"}/>
+            <VerticalCardProduct category={"refrigerator"} heading={"Top's Refrigerator"}/>
+            <VerticalCardProduct category={"mouse"} heading={"Top's Mouse"}/>
+            <VerticalCardProduct category={"camera"} heading={"Top's Camera"}/>
+            <VerticalCardProduct category={"printers"} heading={"Top's Printer"}/>
+            <VerticalCardProduct category={"processor"} heading={"Top's Processor"}/>
+            <VerticalCardProduct category={"speakers"} heading={"Top's Speaker"}/>
+            <VerticalCardProduct category={"television"} heading={"Top's Television"}/>
+            <VerticalCardProduct category={"earphones"} heading={"Top's Earphone"}/>
+            <VerticalCardProduct category={"mobiles"} heading={"Top's Mobiles"}/>
+            <VerticalCardProduct category={"trimmers"} heading={"Top's Trimmers"}/>
         </div>
     )
 }
